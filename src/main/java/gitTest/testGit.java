08490c69579ba828e671c3e7a394bef1599afae9
Marc Ulloa Palli <mulloapa@emeal.nttdata.com>
2023-08-24 19:32:02 +0200

    3era fotografia, actualizamos el print

--- src/main/java/gitTest/testGit.java
+++ src/main/java/gitTest/testGit.java
@@ -4,7 +4,7 @@ public class testGit {
 
     public static void main (String args[]){
 
-       System.out.println("Hola Git");
+       System.out.println("Hola Git! Actualizamos el print");
 
     }
 }
