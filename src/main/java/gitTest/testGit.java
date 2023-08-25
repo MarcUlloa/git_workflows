96bd48da7003fba3112594bba6bf7afdd61e089c
Marc Ulloa Palli <mulloapa@emeal.nttdata.com>
2023-08-25 12:37:38 +0200

    Modificamos la rama master

--- src/main/java/gitTest/testGit.java
+++ src/main/java/gitTest/testGit.java
@@ -5,6 +5,7 @@ public class testGit {
     public static void main (String args[]){
 
        System.out.println("Hola Git! Actualizamos el print de nuevo");
+       System.out.println("modificación master");
 
     }
 }
