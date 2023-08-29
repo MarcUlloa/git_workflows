97f2d10d1b3a7127f9b6b3e1e055bdad34fe01c2
Marc Ulloa Palli <mulloapa@emeal.nttdata.com>
2023-08-29 17:24:20 +0200

    nuevo commit para subir al repositorio

--- src/main/java/gitTest/testGit.java
+++ src/main/java/gitTest/testGit.java
@@ -4,7 +4,7 @@ public class testGit {
 
     public static void main (String args[]){
 
-       System.out.println("Hola Git! Actualizamos el print de nuevo");
+       System.out.println("Hola Git! Actualizamos el print de nuevo y de nuevo");
        System.out.println("modificación master");
 
     }
