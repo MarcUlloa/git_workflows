05696f7189081fb1d9084df513079eabdb303977
Marc Ulloa Palli <mulloapa@emeal.nttdata.com>
2023-09-06 11:22:00 +0200

    prueba push desde consola

--- src/main/java/gitTest/testGit.java
+++ src/main/java/gitTest/testGit.java
@@ -5,7 +5,7 @@ public class testGit {
     public static void main (String args[]){
 
        System.out.println("Hola Git! Actualizamos el print de nuevo y de nuevo");
-       System.out.println("modificación master");
+       System.out.println("modificación master, prueba conexion gitpush por consola");
 
     }
 }
